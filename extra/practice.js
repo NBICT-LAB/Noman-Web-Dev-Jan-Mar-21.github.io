e3ee9d95d799066bd71e7d13a16790a37035fb4c
Noman1170 <77617625+Noman1170@users.noreply.github.com>
2021-02-25 04:15:36 -0800

    new

---
+++ extra/practice.js
@@ -0,0 +1,92 @@
+//this is nbict lab's code
+
+/******************* 
+let js ="awesome";
+console .log(js);
+console.log(7-11+19*2);
+***********************/
+/*
+let firstname='ab';
+let lastname='noman';
+console .log(firstname+' '+lastname+'is an IT instructor and entrepreneur.'
++firstname +' founded nbict lab in 2018');
+
+let x =null ;
+console .log(typeof(x));
+
+
+let counttry='bangladesgh';
+let continent='asia';
+let population= 180;
+
+console.log(counttry)
+console.log(continent)
+console.log(population)
+
+let isIland= true;
+let language;
+console.log(typeof(isIland))
+console.log(typeof(country))
+console.log(typeof(language))
+
+
+
+
+//operator precedence
+
+const now =2037
+const ageJonus =now - 1992;
+const ageSara = now - 1998;
+
+console.log(now-1992>now-1998);
+
+const y = 25-10+5;
+console.log(y);
+
+/*
+const massMark = 78;
+const heightMark= 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+const bmiMark = massMark / (heightMark * heightMark);
+const bmiJohn = massJohn / (heightJohn * heightJohn);
+
+const markHeigherBMI = bmiMark > bmiJohn;
+ console.log(bmiMark, bmiJohn , markHeigherBMI);
+*/
+/*
+const firstName = 'jonas';
+const job = 'teacher';
+const birthYear = 1993;
+const currentYear = 2037;
+//i'm jonas, a 30 years old teacher (``baktic)
+/*
+const jonas = "i'm " + firstName + ',a ' + (currentYear - birthYear) + 'years old' + job +'!';
+
+const jonasNew = `I'm ${firstName
+}, a ${currentYear - birthYear} years old ${job}!`;
+console.log(jonas);
+console.log(jonasNew);
+
+console.log('i am \n\ a multiline \n\ string');
+  console.log(`i am
+  a multiline
+  string...`);
+*/
+/*
+const firstName = 'abdullah';
+const lastName = 'noman';
+const topic ='javaScript';
+const institue = 'NBICT LAB';
+const firstYear = '2015';
+const lastYear = '2020';
+const noman = "i am" + firstName + lastName + "i am " 'learning' + topic + 'at' + institue + 'for' + (firstYear - lastYear )+'years' +'!';
+
+console.log(noman);
+*/
+
+console.log(`i am abdullah noman.
+i am learning javaScript at NBICT LAB
+for 5 years`);
+
+ 
